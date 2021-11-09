b6f6dce54e497188f26331e12b5ed27ad7b9546b
Ruan van der Westhuizen <rvanderwesthuizen2@jhb.dvt.co.za>
2021-11-09 13:59:29 +0200

    Initial file setup

---
+++ OmdbSearchMovieDetails/OmdbSearchMovieDetails/OmdbSearchMovieDetails.h
@@ -0,0 +1,18 @@
+//
+//  OmdbSearchMovieDetails.h
+//  OmdbSearchMovieDetails
+//
+//  Created by Ruan van der Westhuizen on 2021/11/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OmdbSearchMovieDetails.
+FOUNDATION_EXPORT double OmdbSearchMovieDetailsVersionNumber;
+
+//! Project version string for OmdbSearchMovieDetails.
+FOUNDATION_EXPORT const unsigned char OmdbSearchMovieDetailsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OmdbSearchMovieDetails/PublicHeader.h>
+
+
