6d030083ba0028595d6fab8d0c1d9990ed4a1c3f
Ruan van der Westhuizen <rvanderwesthuizen2@jhb.dvt.co.za>
2021-12-04 07:02:41 +0200

    Updated MockedMovieDetailsRepository

--- OmdbSearchMovieDetails/OmdbSearchMovieDetailsTests/MockedMovieDetailsRepository.swift
+++ OmdbSearchMovieDetails/OmdbSearchMovieDetailsTests/MockedMovieDetailsRepository.swift
@@ -10,12 +10,18 @@ import Foundation
 
 class MockedMovieDetailsRepository: MovieDetailRepositable {
     var shouldFail = false
+    var shouldFailToFindID = false
     
     func performRequestWith(imdbID: String, completion: @escaping movieDetailsRepositoryResponseBlock) {
         if !shouldFail {
-            completion(.success(MovieDetails(title: "", year: "", rated: "", released: "", runtime: "", genre: "", director: "", writer: "", actors: "", plot: "", language: "", awards: "", poster: "", imdbRating: "", imdbID: "1234", boxOffice: "", type: "", productionStudio: "")))
+            if !shouldFailToFindID {
+                completion(.success(MovieDetails(title: "", year: "", rated: "", released: "", runtime: "", genre: "", director: "", writer: "", actors: "", plot: "", language: "", awards: "", poster: "", imdbRating: "", imdbID: "1234", boxOffice: "", type: "", productionStudio: "", error: "", response: "")))
+            } else {
+                shouldFailToFindID = false
+                completion(.failure(.IDNotFoundError))
+            }
         } else {
-            completion(.failure(customError.repositoryError("An error occurred while trying to retrieve data")))
+            completion(.failure(.serverError))
         }
     }
 }
